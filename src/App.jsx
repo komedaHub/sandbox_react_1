7b2aa41b7ed8ab3139b3289545233001368208de
komedaHub <wearegolden317@gmail.com>
2022-06-14 21:45:59 +0900

    add memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import ReactDOM from "react-dom";
-import { Fragment } from "react";
-import { useState } from "react";
+import { Fragment, useCallback } from "react";
+import { useState, memo } from "react";
 import { useEffect } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./components/CssModules";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
 
-export const App = () => {
+export const App = memo(() => {
+  console.log("Appレンダリング");
   // Stateの定義
   const [num, setNum] = useState(0);
   const onClickButton = () => {
     setNum(num + 1);
   };
 
+  const onClickReset = useCallback(() => {
+    setNum(0);
+  }, []);
+
   useEffect(() => {
-    alert();
+    // alert();
   }, [num]);
   return (
     <Fragment>
@@ -20,8 +28,12 @@ export const App = () => {
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="pink">元気です!</ColoredMessage>
+      <CssModules />
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
+      <hr />
+      <Child1 onClickReset={onClickReset} />
+      <Child4 />
     </Fragment>
   );
-};
+});
